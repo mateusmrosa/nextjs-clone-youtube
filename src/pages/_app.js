a1aeb57a57e72ca3cb44bb8e2cc078a67eed5165
Mateus Martinez rosa <41523047+mateusmrosa@users.noreply.github.com>
2021-11-13 14:15:14 -0300

    carga inicial

---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+/* eslint-disable semi */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/function-component-definition */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      Clone youtube
+    </div>
+  );
+}
